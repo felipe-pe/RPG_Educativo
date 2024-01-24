972cb43a58aa1f1ad255341b83e8e3a59bed82a5
LucasTalen <lucaspaula58227@outlook.com>
2024-01-24 16:38:18 -0300

    Add funcao que ler o desafio apos mudanças

--- main.py
+++ main.py
@@ -7,4 +7,7 @@ def criar_arquivo_de_desafio(nome_do_desafio, codigo_do_desafio):
     with open(f'./desafios/{nome_do_desafio}.py', 'w') as desafio:
         desafio.write(codigo_do_desafio)
 
-  
+def ler_arquivo_de_desafio(nome_do_desafio):
+    with open(f'./desafios/{nome_do_desafio}.py', 'r') as desafio:
+        return desafio.read()
+
