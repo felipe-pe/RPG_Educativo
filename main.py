a0630688dcc67cb2dc8f404f6d040ec70931d1f8
LucasTalen <lucaspaula58227@outlook.com>
2024-01-24 16:25:04 -0300

    Add função que  criar o arquivo do desafio

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import subprocess
 def abrir_editor(editor,arquivo):
     subprocess.run([editor,arquivo])
     
+def criar_arquivo_de_desafio(nome_do_desafio, codigo_do_desafio):
+    with open(f'./desafios/{nome_do_desafio}.py', 'w') as desafio:
+        desafio.write(codigo_do_desafio)
 
-    
-    
+  
