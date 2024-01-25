99c04afd5a188f997bb2b109ae2e68fd35d4c82d
Eliton lima <elitonlima2@hotmail.com>
2024-01-25 17:38:29 -0300

    inseri um desafio condicional e um txt

---
+++ desafios/condicinal/desafio_condicional02.py
@@ -0,0 +1,20 @@
+# UM TIME DE FUTEBOL PRECISA DIVIDIR SEUS ATLETAS DE ACORDO COM SUAS IDADES
+# A IDADE MENOR QUE 10 É INFANTIL
+# A IDADE MENOR QUE 18 JUVENIL 
+# A IDADE MAIOR OU IGUAL A 18 ADULTO
+# A IDADE MAIOR QUE 40 VETERANO
+
+
+
+idade = 18
+# qual será a resposta?
+# faça a correção para ter o resultado esperado.
+if idade <= 10:
+    print('infantil')
+elif idade < 18;
+    print('juvenil')
+elif idade > 18 and idade < 40:
+    print('adulto')
+else:
+    print('veterano')
+
